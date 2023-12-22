6a81a8ccadcc1cc06a44fd9cc524b8cd2158455d
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2023-12-22 09:58:20 +0900

    allow bootstrap_list to directly handle dfidx

--- R/bootstrap_list.R
+++ R/bootstrap_list.R
@@ -6,6 +6,9 @@
 #' @param x The original data frame.
 #' @param n The number of bootstrapped data frames to return.
 #' @param rows The number of rows to sample from the original data frame.
+#' @param dfidx If TRUE, use the dfidx::dfidx function to transform into mlogit
+#' data format, i.e., a dataframe with indexes. Default is FALSE.
+#' @param ... Additional arguments to pass to dfidx::dfidx.
 #'
 #' @return A list of data frames.
 #'
@@ -15,10 +18,16 @@
 #'
 #' @export
 
-bootstrap_list <- function(x, n = 1000, rows = 1000) {
+bootstrap_list <- function(x, n = 1000, rows = 1000,
+                           dfidx = FALSE, ...) {
   df_list <- vector("list", n)
   for (i in 1:n) {
     df_list[[i]] <- x[sample(nrow(x), replace = TRUE), ]
+    ## If dfidx is TRUE,
+    ## use the dfidx::dfidx function to transform into mlogit format.
+    if (dfidx == TRUE) {
+      df_list[[i]] <- dfidx::dfidx(df_list[[i]], ...)
+    }
   }
   return(df_list)
 }
