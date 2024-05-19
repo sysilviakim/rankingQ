f92360498dfa4df00a1fa96b7b1536421bec9376
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2024-05-19 20:36:49 +0900

    long/wide input consistency across functions

--- R/item_to_rank.R
+++ R/item_to_rank.R
@@ -14,7 +14,7 @@
 #' the columns. If not specified, will use the first 26 letters of the alphabet.
 #' Default is `NULL`.
 #' @param long Whether to return the output in a long data format.
-#' Unless `TRUE`, will not be. Default is `NULL`.
+#' Default is `FALSE`.
 #'
 #' @return A data frame with items that are being ranked as columns,
 #' with rankings in cell values.
@@ -36,7 +36,11 @@
 item_to_rank <- function(item_rank,
                          format_input = "ordering",
                          reference = NULL,
-                         long = NULL) {
+                         long = FALSE) {
+  if (long != FALSE & long != TRUE) {
+    stop("The 'long' argument must be either TRUE or FALSE.")
+  }
+
   if (!is.null(reference)) {
     item_crosswalk <- data.frame(
       name = reference, item_no = paste0("Item_", seq(length(letters)))
