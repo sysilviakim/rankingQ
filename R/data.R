fe7939f8860ce8bf29d209f3c3344d537c84b5cc
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2024-11-22 04:10:04 +0900

    no back-and-forth renaming

--- R/data.R
+++ R/data.R
@@ -17,22 +17,22 @@
 #' to correct for measurement error bias.
 #'
 #' @format ## `identity`
-#' A data frame with 1,082 rows and 12 columns:
+#' A data frame with 1,082 rows and 16 columns:
 #' \describe{
-#'   \item{app_party}{Marginal ranking for party (main identity ranking question).}
-#'   \item{app_religion}{Marginal ranking for religion (main identity ranking question).}
-#'   \item{app_gender}{Marginal ranking for gender (main identity ranking question).}
-#'   \item{app_race}{Marginal ranking for race (main identity ranking question).}
-#'   \item{anc_house}{Marginal ranking for household (anchor question).}
-#'   \item{anc_neighborhood}{Marginal ranking for neighborhood (anchor question).}
-#'   \item{anc_city}{Marginal ranking for city (anchor question).}
-#'   \item{anc_state}{Marginal ranking for state (anchor question).}
+#'   \item{app_identity}{Full ranking profile for the main identity ranking question.}
+#'   \item{app_identity_1}{Marginal ranking for party (main identity ranking question).}
+#'   \item{app_identity_2}{Marginal ranking for religion (main identity ranking question).}
+#'   \item{app_identity_3}{Marginal ranking for gender (main identity ranking question).}
+#'   \item{app_identity_4}{Marginal ranking for race (main identity ranking question).}
+#'   \item{anc_identity}{Full ranking profile for the anchor ranking question.}
+#'   \item{anc_identity_1}{Marginal ranking for household (anchor question).}
+#'   \item{anc_identity_2}{Marginal ranking for neighborhood (anchor question).}
+#'   \item{anc_identity_3}{Marginal ranking for city (anchor question).}
+#'   \item{anc_identity_4}{Marginal ranking for state (anchor question).}
 #'   \item{anc_correct_identity}{Whether the respondent answered the anchor questions correctly. This is a binary variable that 1 if the respondent correctly answers the anchor ranking question and 0 if otherwise.}
 #'   \item{app_identity_recorded}{Recorded responses for the main identity ranking question.}
 #'   \item{anc_identity_recorded}{Recorded responses for the anchor ranking question.}
-#'   \item{app_identity}{Full ranking profile for the main identity ranking question.}
 #'   \item{app_identity_row_rnd}{The order in which the items were randomly presented for the respondent in the main ranking question.}
-#'   \item{anc_identity}{Full ranking profile for the anchor ranking question.}
 #'   \item{anc_identity_row_rnd}{The order in which the items were randomly presented for the respondent in the anchor ranking question.}
 #'   \item{s_weight}{Survey weight.}
 #' }
@@ -49,26 +49,25 @@
 #' provided, united into a single column.
 #'
 #' @format ## `identity_w`
-#' A data frame with 1,082 rows and 14 columns:
+#' A data frame with 1,082 rows and 17 columns:
 #' \describe{
 #'   \item{w}{Estimated weight based on inverse probability weighting.}
-#'   \item{ranking}{Ranking pattern that the respondent provided.}
-#'   \item{party}{Marginal ranking for party (main identity ranking question).}
-#'   \item{religion}{Marginal ranking for religion (main identity ranking question).}
-#'   \item{gender}{Marginal ranking for gender (main identity ranking question).}
-#'   \item{race}{Marginal ranking for race (main identity ranking question).}
-#'   \item{anc_house}{Marginal ranking for household (anchor question).}
-#'   \item{anc_neighborhood}{Marginal ranking for neighborhood (anchor question).}
-#'   \item{anc_city}{Marginal ranking for city (anchor question).}
-#'   \item{anc_state}{Marginal ranking for state (anchor question).}
+#'   \item{s_weight}{Survey weight.}
+#'   \item{app_identity}{Full ranking profile for the main identity ranking question.}
+#'   \item{app_identity_1}{Marginal ranking for party (main identity ranking question).}
+#'   \item{app_identity_2}{Marginal ranking for religion (main identity ranking question).}
+#'   \item{app_identity_3}{Marginal ranking for gender (main identity ranking question).}
+#'   \item{app_identity_4}{Marginal ranking for race (main identity ranking question).}
+#'   \item{anc_identity}{Full ranking profile for the anchor ranking question.}
+#'   \item{anc_identity_1}{Marginal ranking for household (anchor question).}
+#'   \item{anc_identity_2}{Marginal ranking for neighborhood (anchor question).}
+#'   \item{anc_identity_3}{Marginal ranking for city (anchor question).}
+#'   \item{anc_identity_4}{Marginal ranking for state (anchor question).}
 #'   \item{anc_correct_identity}{Whether the respondent answered the anchor questions correctly. This is a binary variable that 1 if the respondent correctly answers the anchor ranking question and 0 if otherwise.}
 #'   \item{app_identity_recorded}{Recorded responses for the main identity ranking question.}
 #'   \item{anc_identity_recorded}{Recorded responses for the anchor ranking question.}
-#'   \item{app_identity}{Full ranking profile for the main identity ranking question.}
 #'   \item{app_identity_row_rnd}{The order in which the items were randomly presented for the respondent in the main ranking question.}
-#'   \item{anc_identity}{Full ranking profile for the anchor ranking question.}
 #'   \item{anc_identity_row_rnd}{The order in which the items were randomly presented for the respondent in the anchor ranking question.}
-#'   \item{s_weight}{Survey weight.}
 #' }
 #' @source <https://github.com/sysilviakim/ranking_error>
 "identity_w"
