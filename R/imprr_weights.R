ee9e27b2e89081cd571538b04360e70bd3aa74f3
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2024-11-25 22:58:04 +0900

    wtd.table parameters must be specified

--- R/imprr_weights.R
+++ R/imprr_weights.R
@@ -175,12 +175,25 @@ imprr_weights <- function(data,
   )
 }
 
-#' Copied from questionr::wtd.table
-#' Not exported
+#' Weighted one-way and two-way frequency tables.
+#'
+#' Copied from questionr::wtd.table. Not exported
+#'
+#' @param x	a vector
+#' @param y	another optional vector for a two-way frequency table. Must be the same length as x
+#' @param weights	 vector of weights, must be the same length as x
+#' @param digits Number of significant digits.
+#' @param normwt if TRUE, normalize weights so that the total weighted count is the same as the unweighted one
+#' @param useNA whether to include NA values in the table
+#' @param na.rm	 (deprecated) if TRUE, remove NA values before computation
+#' @param na.show	 (deprecated) if TRUE, show NA count in table output
+#' @param exclude values to remove from x and y. To exclude NA, use na.rm argument.
 
 wtd.table <- function(
     x, y = NULL, weights = NULL, digits = 3, normwt = FALSE,
-    useNA = c("no", "ifany", "always"), na.rm = TRUE, na.show = FALSE, exclude = NULL) {
+    useNA = c("no", "ifany", "always"), na.rm = TRUE,
+    na.show = FALSE, exclude = NULL) {
+
   if (is.null(weights)) {
     warning("no weights argument given, using uniform weights of 1")
     weights <- rep(1, length(x))
