9c087dd176a7581e1ecd28c37f170d9f45d93058
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2024-11-25 22:36:45 +0900

    no need to have table as argument in uniformity_test

--- R/uniformity_test.R
+++ R/uniformity_test.R
@@ -6,8 +6,6 @@
 #' @importFrom stats chisq.test
 #'
 #' @param data The input dataset with ranking data.
-#' @param table Logical value indicating whether the data
-#' is already in a table format. Defaults to FALSE.
 #' @param var The variable within \code{data} to be used in the test.
 #' Defaults to NULL.
 #'
@@ -15,35 +13,28 @@
 #'
 #' @export
 
-uniformity_test <- function(data, table = FALSE, var = NULL) {
+uniformity_test <- function(data, var = NULL) {
   ## Argument type check
   if (!is.null(var) & !is.character(var)) {
     stop("The 'var' argument must be a character string.")
   }
-  if (!is.logical(table)) {
-    stop("The 'table' argument must be a logical value.")
-  }
-  if (!is.data.frame(data)) {
-    stop("The 'data' argument must be a data frame.")
-  }
 
-  ## Sanity test
-  if (!is.null(var) & !var %in% names(data)) {
-    stop("The variable specified is not in the data.")
-  }
-
-  if (isFALSE(table) & is.null(var)) {
+  if (!("table" %in% class(data)) & is.null(var)) {
     stop(
       paste0(
         "If the data is not already in a table format, ",
         "please specify the variable to be used in the test."
       )
     )
-  } else if (isFALSE(table) & !is.null(var)) {
-    tab <- table(data[[var]])
-    tab <- permn_augment(tab)
-    return(chisq.test(tab, p = rep(1 / length(tab), length(tab))))
-  } else if (isTRUE(table)) {
+  } else if (!("table" %in% class(data)) & !is.null(var)) {
+    if (!(var %in% names(data))) {
+      stop("The variable specified is not in the data.")
+    } else {
+      tab <- table(data[[var]])
+      tab <- permn_augment(tab)
+      return(chisq.test(tab, p = rep(1 / length(tab), length(tab))))
+    }
+  } else if ("table" %in% class(data)) {
     tab <- permn_augment(tab)
     return(chisq.test(data, p = rep(1 / length(data), length(data))))
   }
